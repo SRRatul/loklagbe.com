b9fb434ee3428b1b15f68e43fe2d40d9f5b44fac
VertibirdPilot <rcjuice69@gmail.com>
2025-03-04 02:40:39 +0600

    created login page

---
+++ Script.js
@@ -0,0 +1,9 @@
+document.getElementById('facebook-btn').addEventListener('click', function() {
+    alert('Redirecting to Facebook login...');
+    // Here you can add code for redirection or authentication with Facebook.
+});
+
+document.getElementById('google-btn').addEventListener('click', function() {
+    alert('Redirecting to Google login...');
+    // Here you can add code for redirection or authentication with Google.
+});
